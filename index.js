9f84e8455a7ad5d9f52cb67f7bed867687fbfdc2
SUMIT KUMAR <iam.sumitkumar64@gmail.com>
2025-03-08 20:10:05 +0530

    Help To Do Something In Time With Break Mode On Fix Some bugs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.getElementById('mode').addEventListener("click", () => {
     } else {
         document.body.setAttribute('style', 'color:white;background-color: rgb(0,0,0);');
     }
-})
+});
 
 let label = document.getElementsByTagName('label');
 const cont = document.getElementById("cont");
@@ -12,16 +12,18 @@ const s_inc = document.getElementById("s_inc");
 const s_dec = document.getElementById("s_dec");
 const b_inc = document.getElementById("b_inc");
 const b_dec = document.getElementById("b_dec");
+const start = document.getElementById("start");
 const pause = document.getElementById("pause");
 const reset = document.getElementById("reset");
-let s = 10, b = 5, s_min = 10, sec = 59, b_min = 5, s_obj, b_obj;
-let flag = false;
 
-function toggleButtons(disable) {
-    s_inc.disabled = disable;
-    s_dec.disabled = disable;
-    b_inc.disabled = disable;
-    b_dec.disabled = disable;
+let s = 10, b = 5, s_min = s, sec = 0, b_min = b, session = true, obj;
+let flag = true, r_sec = 0, r_min = 0;
+
+function toggleButtons(ans) {
+    s_inc.disabled = ans;
+    s_dec.disabled = ans;
+    b_inc.disabled = ans;
+    b_dec.disabled = ans;
 }
 
 function s_inc_func() {
@@ -29,9 +31,9 @@ function s_inc_func() {
     s_min = s;
     label[1].innerText = s;
 }
+
 function s_dec_func() {
-    if (s == 0) { s = s; }
-    else {
+    if (s > 0) {
         s--;
         s_min = s;
         label[1].innerText = s;
@@ -43,9 +45,9 @@ function b_inc_func() {
     b_min = b;
     label[2].innerText = b;
 }
+
 function b_dec_func() {
-    if (b == 0) { b = b; }
-    else {
+    if (b > 0) {
         b--;
         b_min = b;
         label[2].innerText = b;
@@ -57,61 +59,76 @@ s_dec.addEventListener("click", s_dec_func);
 b_inc.addEventListener("click", b_inc_func);
 b_dec.addEventListener("click", b_dec_func);
 
-start.addEventListener("click", () => {
-    if (flag == false) {
-        toggleButtons(true);
-        flag = true;
-        s_obj = setInterval(() => {
-            if (s_min >= 0) {
-                label[0].innerText = `${s_min < 10 ? '0' + s_min : s_min}:${sec < 10 ? '0' + sec : sec}`;
-                sec--;
-                label[0].style.border = '1px solid rgb(89, 230, 235)';
-                if (sec < 0) {
-                    sec = 60;
-                    s_min = s_min - 1;
-                }
+function startfunc(minute, second) {
+    obj = setInterval(() => {
+        if (minute > 0 || second > 0) {
+            label[0].innerText = `${minute < 10 ? '0' + minute : minute}:${second < 10 ? '0' + second : second}`;
+            label[0].style.border = session ? '1px solid rgb(0, 89, 255)' : '1px solid rgb(255, 0, 0)';
+            second--;
+            if (second < 0) {
+                second = 59;
+                minute--;
             }
-            else {
-                label[0].innerText = s_min + sec;
+            r_min = minute;
+            r_sec = second;
+        } else {
+            session = !session;
+            pausefunc();
+            if (session) {
+                s_min = s;
+                sec = 0;
+                cont.innerText = "Session Time";
+            } else {
+                b_min = b;
+                sec = 0;
                 cont.innerText = "Break Time";
-                pausefunc();
-                b_obj = setInterval(() => {
-                    if (b_min >= 0) {
-                        label[0].innerText = `${b_min < 10 ? '0' + b_min : b_min}:${sec < 10 ? '0' + sec : sec}`;
-                        label[0].style.border = '1px solid rgb(255, 0, 0)';
-                        sec--;
-                        if (sec < 0) {
-                            sec = 60;
-                            b_min = b_min - 1;
-                        }
-                    } else { b_min = b; }
-                }, 10);
             }
-        }, 10);
+            startfunc(session ? s_min : b_min, sec);
+        }
+    }, 10);
+}
+
+start.addEventListener("click", () => {
+    toggleButtons(true);
+    if (flag == true) {
+        if (s_min != 0) {
+            if (r_min == 0 && r_sec == 0) {
+                startfunc(s_min, sec);
+            } else {
+                startfunc(r_min, r_sec);
+            }
+            flag = false;
+        } 
+        else {
+            alert("Please Set Session Time");
+            toggleButtons(false);
+        }
+    } else {
+        alert("Start Already Working");
     }
 });
 
 function pausefunc() {
-    clearInterval(s_obj);
-    clearInterval(b_obj);
-    flag = false;
+    clearInterval(obj);
     toggleButtons(false);
+    flag = true;
 }
+
 pause.addEventListener("click", pausefunc);
 
 function resetfunc() {
-    clearInterval(s_obj);
-    clearInterval(b_obj);
-    flag = false;
+    clearInterval(obj);
     toggleButtons(false);
+    flag = true;
     s = 10;
     b = 5;
     s_min = 10;
-    sec = 59;
+    sec = 0;
     b_min = 5;
     label[1].innerText = s;
     label[2].innerText = b;
     cont.innerText = "Session Time";
     label[0].innerText = "00:00";
 }
+
 reset.addEventListener("click", resetfunc);
